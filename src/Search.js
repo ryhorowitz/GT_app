f285173d204f6132d75fee2cf305e0806305d73e
ryhorowitz <ryan.w.horowitz@gmail.com>
2022-06-13 15:09:01 -0400

    New set up with dummy form. Starter state.

---
+++ src/Search.js
@@ -0,0 +1,36 @@
+import React from 'react';
+
+class Search extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = { value: '' };
+
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleChange(e) {
+        this.setState({ value: e.target.value })
+    }
+
+    handleSubmit(e) {
+        alert('A name was submitted: ' + this.state.value);
+        //hit the server with value
+        e.preventDefault();
+    }
+
+    render() {
+        return (
+            <form onSubmit={this.handleSubmit}>
+                <label>
+                    Name:
+                    <input type="text" value={this.state.value} onChange={this.handleChange} />
+                </label>
+                <input type="submit" value="Submit" />
+            </form>
+        );
+    }
+}
+
+
+export default Search;
