c38fd09b1e55c3e8a149c094bc06aaea6761a0c9
ryhorowitz <ryan.w.horowitz@gmail.com>
2022-06-27 17:18:46 -0400

    Building add case from. Debating wether or not to add React Hook Form Dep.

--- src/App.js
+++ src/App.js
@@ -2,12 +2,15 @@ import './App.css';
 import Search from './components/Search';
 import GetAll from './components/GetAll';
 import GetAllButton from './components/GetAllButton';
+import AddCase from './components/AddCase';
 
 function App() {
 
   return (
     <div className="App">
       <header className="App-header">
+        <AddCase/>
+        <br></br>
         <Search/>
         <GetAllButton/>
         {/* <GetAll/> */}
