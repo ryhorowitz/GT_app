e13b6b40e56de72eb34ab7e1943a84bfcd7913d5
ryhorowitz <ryan.w.horowitz@gmail.com>
2022-07-01 18:27:12 -0400

    need to research fetch get request queries.

--- src/components/Search.js
+++ src/components/Search.js
@@ -2,19 +2,20 @@ import React, { useState } from 'react';
 import { useForm } from 'react-hook-form';
 
 function Search() {
-   
-  const {register, handleSubmit } = useForm();
-  
+
+  const { register, handleSubmit } = useForm();
+
   const findCase = (caseNumber) => {
-    // fetch to new address
-    fetch('http://localhost:3000/search')
-    // then
-    .then( res => res.json)
-    .then( data => console.log('case info:', data))
-    .catch( err => console.error('ERROR:', err))
+    console.log('caseNumber:', caseNumber)
+    // 
+    fetch(`http://localhost:3000/search${caseNumber}`)
+      // then
+      .then(res => res.json)
+      .then(data => console.log('case info:', data))
+      .catch(err => console.error('ERROR:', err))
   }
   return (
-    <form>
+    <form onSubmit={handleSubmit(findCase)}>
       <h2>Search Form</h2>
       <label>Case Number:</label>
       <input
