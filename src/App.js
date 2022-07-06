23579a8f9af33a82bebe01d5888bed3484d7b5e2
ryhorowitz <ryan.w.horowitz@gmail.com>
2022-07-06 18:32:47 -0400

    Router set up but not how I want it.

--- src/App.js
+++ src/App.js
@@ -1,26 +1,16 @@
 import './App.css';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import logo from "./img/GT-Logo.png"
-import Search from './components/Search';
-import GetAllButton from './components/GetAllButton';
-import AddCase from './components/AddCase';
+import { Outlet, Link } from 'react-router-dom';
 
 function App() {
-
   return (
-    <div className="App">
-      <header className="App-header">
-        <AddCase/>
-        <br></br>
-        <Search/>
-        <br></br>
-        <br></br>
-        <GetAllButton/>
-      </header>
-      {/* <div>
-        <img className="logo" src={logo} alt="GT-Logo"></img>
-      </div> */}
-    </div>
+    <>
+    <h1>GT Case Status App</h1>
+    <nav>
+      <Link to='/search'>Search</Link>
+      <Link to='/add-case'>Add Case</Link>
+    </nav>  
+    <Outlet/>
+    </>
   );
 }
 
