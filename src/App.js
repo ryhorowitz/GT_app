1cebfc9077a477f8a44a8653e170e27f4fa9871b
ryhorowitz <ryan.w.horowitz@gmail.com>
2022-07-23 18:59:03 -0400

    Nested React router for search by implemented.

--- src/App.js
+++ src/App.js
@@ -2,7 +2,9 @@ import './App.css';
 import Navbar from '../src/components/Navbar';
 import Home from './components/Home';
 import AddCase from './components/AddCase';
-import Search from './components/Search';
+import Search from './components/search/Search';
+import CaseFileSearch from './components/search/CaseFileSearch';
+import NameSearch from './components/search/NameSearch';
 import { Route, Routes } from "react-router-dom";
 function App() {
   return (
@@ -12,8 +14,12 @@ function App() {
     <div className='container'>
       <Routes>
         <Route path='/' element={<Home />} />
-        <Route path='/add-case' element={<AddCase />} />
-        <Route path='/search' element={<Search />} />
+        <Route path='add-case' element={<AddCase />} />
+        <Route path='search' element={<Search />}>
+          <Route path='name' element={<NameSearch />}/>
+          <Route path='case-number' element={<CaseFileSearch />}/>
+        </Route>
+
       </Routes>
     </div>
     </>
--- src/components/search/Search.js
+++ src/components/search/Search.js
@@ -1,18 +1,19 @@
 import React, { useState } from 'react';
 // import { useForm } from 'react-hook-form';
-import { Link } from 'react-router-dom';
+import { Link, Outlet } from 'react-router-dom';
 
 
 function Search() {
 
   return (
     <>
-     <h2>Search By</h2>
+     <h2>Search By:</h2>
       <ul className='search-options'>
-        <Link to='name'>Name</Link>
-        or
         <Link to='case-number'>Case Number</Link>
+        or
+        <Link to='name'>Name</Link>
       </ul>
+      <Outlet />
     </>
   )
 }
